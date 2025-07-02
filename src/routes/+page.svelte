e79a01406bd3e2d49ef339e032f9a109dade1707
BorisSavianov <bsavianov@gmail.com>
2025-07-02 14:02:25 +0300

    test api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>This is the api. What are you doing here?</h1>
